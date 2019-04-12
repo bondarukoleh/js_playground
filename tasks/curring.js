da427e8bb98f405e82bcbccfa820a77fc80500ff
Oleh.Bondaruk <bondaruk9000@gmail.com>
2019-04-12 07:27:23 +0300

    trick with curring added

--- tasks/curring.js
+++ tasks/curring.js
@@ -1,6 +1,18 @@
-// sum(2,3) || sum(2)(3) -> 5
-const cur = (num1, num2) => {
+// curring(2,3) || curring(2)(3) -> 5
+const curring = (num1, num2) => {
   return num2 ? num1 + num2 : (num2) => num1 + num2
 }
-console.log(cur(2)(3) == 5);
-console.log(cur(2,3) == 5);
+console.log(curring(2)(3) == 5);
+console.log(curring(2,3) == 5);
+
+// curring(2)(3) || curring(1)(2)(2) -> 5 || curring(1)(1)(1)(2) -> 5
+function curring2(num){
+  let sum = num
+  const f = (nextParameter) => {
+    sum += nextParameter
+    return f
+  }
+  f.valueOf = _ => sum
+  return f
+}
+console.log(curring2(2)(3) == 5, curring2(1)(2)(2) == 5, curring2(1)(1)(1)(2) == 5);
