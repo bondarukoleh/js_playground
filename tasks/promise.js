65eb5bc884a9f08d2a7be60b6916bb1c4aa31cb1
Oleh.Bondaruk <bondaruk9000@gmail.com>
2019-04-13 03:28:32 +0300

    promise sequense improved

--- tasks/promise.js
+++ tasks/promise.js
@@ -1,39 +1,59 @@
 const thenThing = _ => {
-  const promise = function(value, time = 1000){
-    return new Promise(function(resolve, reject){
+  const promise = function (value, time = 1000) {
+    return new Promise(function (resolve, reject) {
       setTimeout(resolve, time, value)
     })
   }
 
   promise(5, 500)
-  .then((value) => {
-    console.log(value);
-    return 'returned from then'
-  })
-  .then((value) => {
-    console.log(value);
-    throw new Error('Error from then')
-  })
-  .then(_ => {},(err) => {
-    console.log(err.message);
-    return 'value after error'
-  })
-  .then(val => {
-    console.log(val);
-  })
-  .catch(console.log)
+    .then((value) => {
+      console.log(value);
+      return 'returned from then'
+    })
+    .then((value) => {
+      console.log(value);
+      throw new Error('Error from then')
+    })
+    .then(_ => { }, (err) => {
+      console.log(err.message);
+      return 'value after error'
+    })
+    .then(val => {
+      console.log(val);
+    })
+    .catch(console.log)
 }
 
 const promiseSequense = _ => {
-  const promise = function(value, time = 1000){
-    return new Promise(function(resolve, reject){
-      setTimeout(() => {
-        console.log(`resolving ${value}`);
-        resolve(value)
-      }, time)
-    })
-  };
+  const useValueOfPreviousPromiseInNextOne = _ => {
+    const firePromise = function (value, time = 1000) {
+      return new Promise(function (resolve, reject) {
+        setTimeout(() => {
+          console.log(`resolving ${value}`);
+          resolve(value)
+        }, time)
+      })
+    };
 
-  [1, 2, 3].reduce((acc, cur) => acc.then(val => promise(val + cur)), Promise.resolve(0))
+    [1, 2, 3].reduce((result, cur) => result.then((value) => firePromise(value + cur)), Promise.resolve(0))
+  }
+  useValueOfPreviousPromiseInNextOne()
+  
+  const resolvePromiseOnebyAnother = _ => {
+    const getPromise = () => new Promise(function (resolve, reject) {
+      setTimeout(() => { console.log(`resolving some promise`); resolve() }, 1000)
+    })
+    const arrOfPromises = [getPromise, getPromise, getPromise]
+
+    // via forEach
+    let result = Promise.resolve();
+    arrOfPromises.forEach((currentPromise) => {
+      result = result.then(currentPromise)
+    })
+
+    // via reduce
+    arrOfPromises.reduce((acc, cur) => acc.then(cur), Promise.resolve())
+  }
+  resolvePromiseOnebyAnother()
 }
 promiseSequense()
