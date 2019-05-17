9e84e869611f3ebbb70ef70281307b30504d4163
Oleh <bondaruk9000@gmail.com>
2019-05-17 14:25:43 +0300

    continue with scope and context

--- functions/tries.js
+++ functions/tries.js
@@ -7,3 +7,35 @@ const f = () => 1;
 // aa() // not error because we invoking aa has access to [[outer]] scope where f is already initialized.
 // console.log(a)
 
+// global scope
+const func = function(){
+  // func scope:
+    // [[Environment]]: func scope
+    // innerValue
+    // inner
+    // [[Outer]] -> global scope
+  const innerValue = 'InnerValue';
+  function inner (){
+    // inner scope:
+    // [[Environment]]: inner scope
+    // [[Outer]] -> func scope
+    console.log(innerValue)
+  }
+  return inner;
+}
+
+const returnedInner = func()
+returnedInner()
+
+/*
+Stack:
+--global--
+
+then
+--func-- // here the [[Outer]] link for inner function is set to func [[Environment]]
+--global--
+
+Then
+--returnedInner-- // [[outer]].innerValue grabbed from func [[Environment]]
+--global--
+*/
