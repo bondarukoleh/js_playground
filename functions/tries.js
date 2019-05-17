de453496cdbe22ba4500bc4ed598f893c0359132
Oleh <bondaruk9000@gmail.com>
2019-05-17 18:17:35 +0300

    continue with scope and context tricks

--- functions/tries.js
+++ functions/tries.js
@@ -7,25 +7,30 @@ const f = () => 1;
 // aa() // not error because we invoking aa has access to [[outer]] scope where f is already initialized.
 // console.log(a)
 
-// global scope
+// global scope:
+  // [[Environment]]: global scope
+  // value
+  // func
+  // [[Outer]] -> global scope
+const value = 'global value'
 const func = function(){
   // func scope:
     // [[Environment]]: func scope
     // innerValue
     // inner
     // [[Outer]] -> global scope
-  const innerValue = 'InnerValue';
-  function inner (){
+  const value = 'inner Value';
+  function innerFunc (){
     // inner scope:
     // [[Environment]]: inner scope
     // [[Outer]] -> func scope
-    console.log(innerValue)
+    console.log(value)
   }
-  return inner;
+  return innerFunc;
 }
 
-const returnedInner = func()
-returnedInner()
+const returnedInnerFunc = func()
+// returnedInnerFunc() // 'inner Value' because innerFunc created in func
 
 /*
 Stack:
@@ -39,3 +44,23 @@ Then
 --returnedInner-- // [[outer]].innerValue grabbed from func [[Environment]]
 --global--
 */
+
+// another example
+
+function withInnerFunction(){
+  const value = 'First value';
+  function innerFunction(){
+    console.log(value)
+  }
+  const arrowInner = () => console.log(value)
+  return {innerFunction, arrowInner}
+}
+
+function withoutInnerFunction(){
+  const value = 'Second value';
+  const {innerFunction, arrowInner} = withInnerFunction()
+  innerFunction() // 'First value' // because function was CREATED inside withInnerFunction
+  arrowInner() // 'First value' as you can see arrow doesn't have this, but it has same scope behavior 
+}
+
+withoutInnerFunction()
