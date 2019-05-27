3cd6c1bdaea2bcfdf8a9402b14964156dece22f8
Oleh <bondaruk9000@gmail.com>
2019-05-27 15:38:50 +0300

    promise sequence added

--- promise/promise.js
+++ promise/promise.js
@@ -6,3 +6,30 @@ const promise = function (resolvePromise = true) {
 }
 
 promise(false).then(console.log).catch(console.log)
+
+const urls = ['first', 'second', 'third']
+const getUrl = function (url) {
+  return new Promise(function (res, rej) {
+    setTimeout(() => {
+      console.log(`Getting ${url}`);
+      res()
+    }, 1000)
+  })
+}
+urls.reduce((result, curUrl) => {
+  return result.then(_ => {
+    return getUrl(curUrl)
+  })
+}, Promise.resolve())
+
+let valuesArray = [1, 2, 3]
+const addValuePromise = function(value){
+  return new Promise(function(res){
+    setTimeout(() => {
+      console.log(`returning ${value}`)
+      res(value)
+    }, 1000)
+  })
+}
+
+valuesArray.reduce((resolved, cur) => resolved.then((value) => addValuePromise(value + cur)), Promise.resolve(0)).then(console.log)
