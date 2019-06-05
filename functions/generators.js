4a88998227062d44b60db936a553fcdc217d5542
Oleh.Bondaruk <bondaruk9000@gmail.com>
2019-06-05 22:57:58 +0300

    generators dived

--- functions/generators.js
+++ functions/generators.js
@@ -1,8 +1,23 @@
-/* We can pass value to generators, and passed value becomes new value of whole yield expression,
+/* We can pass value to generators, and passed value becomes new value of suspended yield,
 but there is exception:
-We can pass value only to WAITING yield, 
+We can pass value only to SUSPENDED yield, 
 that's why we cannot pass anything to first next, nothing is waiting, no yield is suspended. 
 To pass to first yield, we can use just argument when we initialize generator to iterator. 
+
+Generator has 4 states
+Suspended start -> Execution -> Suspended yield -> Completed
+                       |                | 
+                        <---------------    
+So the execution is going by this flow:
+1. We call generator -> (Suspended start) generator context on the top of the stack, and generators arguments initiated with
+passed values, generator returns the iterator with closure on generator context. After return of iterator - generator context
+is popped out from stack but doesn't removed, since it has iterator has link on it from closure. 
+2. We call iterator.next() -> (Execution) generator context is back in stack, and executing until reaches yield. It return
+whole expression from the left of yield, and becomes suspended (Suspended yield) generator context is popped out from stack
+but doesn't removed since closure.
+3. We pass value to next, iterator.next(value) -> (Execution) suspended yield initiated with the passed value. generator executes
+either to next "yield" - witch set the (Suspended yield) state or "return" - witch set the (Completed) state.
+4. When .next() called and generator execution doesn't face yield (or return) - generator became (Completed).
 */
 const cl = console.log;
 
@@ -10,7 +25,7 @@ const checkPassedValueGenerator = () => {
 	function* twoWayGen(value) {
 		// value -> is bound to passed argument twoWayGen('INITIAL')
 		// in passedThruSecondNext here we can store passed thru next(arg) value
-		cl('Inside the generator');
+		cl('Inside the generator. Printed only after first .next() called.');
 		/* After first next() call, first yield returns string and suspends */
 		/* After second next('PASSED TO 1st yield') call, suspended first yield initiate the variable with arg value,
 		and execution goes on */
@@ -36,7 +51,7 @@ const checkPassedValueGenerator = () => {
 
 	cl('Start');
 	const generator = twoWayGen('INITIAL');
-	cl('Generator is initiated');
+	cl('Generator is initiated. Generator arguments is initiated, but not a singe row is executed.');
 	// we should not pass anything in first next, since nothing is suspended
 	cl(`First return: ${generator.next(`Won't go anywhere`).value}`);
 	cl('---------------------------------------------------------');
@@ -80,3 +95,43 @@ const accumulateValues = () => {
 	cl(`Accumulated values: ${accumulateGen.next('Final').value}`)
 };
 // accumulateValues()
+
+const checkReturn = () => {
+  function* checkYieldAfterReturn(){
+    yield 'start';
+    // return 'final'; /*return sets generator in "Completed state". No matter how much yield is below.*/
+    const passed = yield 'Last yield'; /*last yield doesn't set generator in "Completed state", we can call next once more*/
+    cl('We can do some job below last yield. Printed after 3rd .next() called.')
+    cl(passed);
+  }
+
+  const iterator = checkYieldAfterReturn()
+  cl(iterator.next().value) //start
+  // cl(iterator.next()) //{ value: 'final', done: true }
+  cl(iterator.next().value) //Last yield
+  cl(iterator.next('Passed Arg')) // { value: undefined, done: true }
+}
+checkReturn()
+
+const checkThrow = () => {
+  function* checkYieldAfterThrow(){
+    try{
+      yield 'start';
+      yield 'Last yield'; /*last yield doesn't set generator in "Completed state", we can call next once more*/
+    } catch (e){
+      cl(`Error is thrown from iterator, caught on generator side "${e}"`)
+      throw 'Thrown final'; /*throw sets generator in "Completed state". Throw from generator should be caught on caller side*/
+    }
+  }
+
+  const iterator = checkYieldAfterThrow()
+  try {
+    cl(iterator.next().value) //start
+    /* If there no try/catch in generator, error will be yielded back here, no matter that we throw it from here */
+    cl(iterator.throw('Thrown Arg')) //  { value: undefined, done: true }
+  } catch (e) {
+    cl(`Error is thrown from generator, caught on iterator side "${e}"`)
+    cl(iterator.next()) // { value: undefined, done: true }
+  }
+}
+// checkThrow()
--- promise/promise_generators.js
+++ promise/promise_generators.js
@@ -1,16 +1,46 @@
-const promise = function(value){
-	return new Promise(
-	function (resolve, reject){
-	setTimeout(() => {
-		console.log('promise resolved')
-		resolve(value)
-	}, 500)
-})
-};
+const promise = (value) => new Promise((resolve) => setTimeout(() => resolve(value), 1000));
 
-function* superGenerator(){
-	yield promise('first value')
-};
+const simpleUsage = () => {
+  function* generatePromise() {
+    yield promise('value')
+  };
 
-const generator = superGenerator();
-generator.next().value.then((res)=>{console.log(res)});
+  const generator = generatePromise();
+  generator.next().value.then((res) => { console.log(res) });
+};
+// simpleUsage()
+
+const addValuesAsync = () => {
+  const values = [1, 2, 3]
+  function* generatePromise(valuesToUse) {
+    const firstResult = yield promise(valuesToUse[0])
+    const secondResult = yield promise(valuesToUse[1] + firstResult)
+    const lastResult = yield promise(valuesToUse[2] + secondResult)
+    return lastResult
+  }
+
+  function addValuesFromIterator(iterator) {
+    function handlePromise({ value, done }) {
+      if (done) {
+        console.log(`Work done, result is ${value}`);
+        console.log('Thank god we have async/await.');
+        return value;
+      }
+      if (value instanceof Promise) {
+        value
+          .then((resolvedValue) => {
+            console.log(`We've got: ${resolvedValue}`);
+            handlePromise(iterator.next(resolvedValue))
+          })
+          .catch((err) => iterator.throw(err))
+      } else {
+        throw Error('Something goes wrong. Check the code.')
+      }
+    }
+    return handlePromise(iterator.next())
+  }
+
+  const iterator = generatePromise(values)
+  addValuesFromIterator(iterator)
+}
+addValuesAsync()
