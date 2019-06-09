00bb2f0ac9d86644d21869723073cfe6b3643624
Oleh.Bondaruk <bondaruk9000@gmail.com>
2019-06-10 00:14:34 +0300

    refactored 

--- class/checkType.js
+++ class/checkType.js
@@ -1,67 +1,66 @@
 cl = console.log;
-function showProp(obj){
-	for(const p in obj){
-		cl('prop ', p, ' is enumerable')
-	}
+function showProperties(obj) {
+  for (const p in obj) {
+    cl(`Property: "${p}" is enumerable.`)
+  }
 }
-function checkClassType () {
-class A {
-	constructor(){
-		this.aProp = 'A property';
-	}
-}
+function checkClassType() {
+  class A {
+    constructor() {
+      this.aProp = 'A property';
+    }
+  }
 
-class B extends A {
-	constructor(){
-		super()
-	  this.bProp = 'B property';
-	}
-}
+  class B extends A {
+    constructor() {
+      super()
+      this.bProp = 'B property';
+    }
+  }
 
-const b = new B();
-cl(b instanceof B);
-cl(b instanceof A);
-cl(b.constructor === B);
-cl(b.constructor === A);
-cl(b.constructor);
-cl(B.prototype.__proto__.constructor);
-cl('aProp in b', 'aProp' in b)
-cl('bProp ownProperty in b', b.hasOwnProperty('aProp'));
-cl('bProp ownProperty in b', b.hasOwnProperty('bProp'));
-const a = new B.prototype.__proto__.constructor();
-cl(a instanceof A);
+  const b = new B();
+  cl(`b instanceof B`, b instanceof B);
+  cl(`b instanceof A`, b instanceof A);
+  cl(`b.constructor === B`, b.constructor === B);
+  cl(`b.constructor === A`, b.constructor === A);
+  cl(`b.constructor`, b.constructor);
+  cl(`B.prototype.__proto__.constructor`, B.prototype.__proto__.constructor);
+  cl('aProp in b', 'aProp' in b)
+  cl('b.hasOwnProperty aProp in b', b.hasOwnProperty('aProp'));
+  cl('b.hasOwnProperty bProp in b', b.hasOwnProperty('bProp'));
+  const a = new B.prototype.__proto__.constructor();
+  cl(`a instanceof A created from __proto__.constructor`, a instanceof A);
 }
 // checkClassType()
 
- function A(){
- 	this.aProp = 'aProp'
- }
- A.prototype.cMethod = () => cl('C method')
- 
- function B (){
- 	A.call(this)
- 	 cl('c m in this', this.__proto__.cMethod)
- 	 cl('b m in this',this.__proto__.bMethod)
- 	 cl(this.aProp)
- 	this.bProp = 'bProp'
- }
- B.prototype=Object.create(A.prototype); 
- //B.prototype.constructor = B; value will be enumerable, which is wrong
- Object.defineProperty(B.prototype, 'constructor', {configurable:true, enumerable:false, value:B})
- showProp(B.prototype)
- B.prototype.bMethod = () => cl('B method');
- //B.prototype.__proto__ = A.prototype cheat
-const b = new B();
-cl('b instanceof B',  b instanceof B);
-cl('b instanceof A', b instanceof A);
-cl('b constr = B', b.constructor === B);
-cl('b constr = A',b.constructor === A);
-cl('b constr', b.constructor);
-cl('b prototype proto construct',B.prototype.__proto__.constructor);
-cl('aProp in b', 'aProp' in b)
-cl('aProp ownProperty in b', b.hasOwnProperty('aProp'));
-cl('bProp ownProperty in b', b.hasOwnProperty('bProp'));
-const a = new B.prototype.__proto__.constructor();
-cl('a instanceof A', a instanceof A);
- 
- 
+function checkFunctionType() {
+  function A() {
+    this.aProp = 'aProp'
+  }
+  A.prototype.aMethod = () => cl('A method')
+
+  function B() {
+    A.call(this)
+    this.bProp = 'bProp'
+  }
+  //B.prototype.constructor = B; value will be enumerable, which is wrong
+  // Object.defineProperty(B.prototype, 'constructor', { configurable: true, enumerable: false, value: B }) - we can do 
+  // this way, but why if we have ability to 
+  B.prototype = Object.create(A.prototype, {constructor: { configurable: true, enumerable: false, value: B }});
+  B.prototype.bMethod = () => cl('B method');
+  showProperties(B.prototype)
+  // B.prototype.__proto__ = A.prototype //cheat, but the same result
+  const b = new B();
+  cl('b instanceof B', b instanceof B);
+  cl('b instanceof A', b instanceof A);
+  cl('b constr = B', b.constructor === B);
+  cl('b constr = A', b.constructor === A);
+  cl('b constr', b.constructor);
+  cl('b prototype proto construct', B.prototype.__proto__.constructor);
+  cl('aProp in b', 'aProp' in b)
+  cl('aProp ownProperty in b', b.hasOwnProperty('aProp'));
+  cl('bProp ownProperty in b', b.hasOwnProperty('bProp'));
+  const a = new B.prototype.__proto__.constructor();
+  cl('a instanceof A', a instanceof A);
+}
+// checkFunctionType()
