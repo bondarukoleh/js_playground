434bb5fcd7c045e5a8f1ee49bdaee304f55ef055
Oleh <bondaruk9000@gmail.com>
2019-06-11 17:27:06 +0300

    some getters setters fixes

--- class/getSet.js
+++ class/getSet.js
@@ -1,62 +1,64 @@
 const cl = console.log;
-function funcGetSet (){
+function funcGetSet() {
 	// old way
-	let innerProp = 'default value'
-	function A (){
-	Object.defineProperties(this, {
-		'innerProp' : {get: () => {
-			console.log('some check');
-			return innerProp;
-		}}, 
-		'innerProp': {set: (value) => {
-			console.log('some check befor setting ', value);
-			innerProp = value
-		}
-	}})
-	} 
+	let innerProperty = 'default value'
+	function A() {
+		Object.defineProperties(this, {
+			innerProp: {
+				get: () => {
+					cl(`some log/check/action before returning "${innerProperty}"`);
+					return innerProperty;
+				},
+				set: (value) => {
+					cl(`some log/check/action before setting "${innerProperty}" to "${value}"`);
+					innerProperty = value;
+				}
+			}
+		})
+	}
 	const a = new A();
-	const aProp = a.innerProp;
-	a.innerProp = 'new value for func';
+	a.innerProp = 'new value';
+	cl(a.innerProp);
 }
-//funcGetSet()
-function objGetSet (){
+funcGetSet()
+function objGetSet() {
 	// in object
-	let innerProp = 'default value'
+	let innerProp = 'default object value'
 	const obj = {
-		get innerProp(){
-			cl('some check before get');
+		get innerProp() {
+			cl(`some log/check/action before returning "${innerProp}"`);
 			return innerProp;
-		}, 
-	set innerProp(value){
-			cl('some check before set ', value);
+		},
+		set innerProp(value) {
+			cl(`some log/check/action before setting "${innerProp}" to "${value}"`);
 			innerProp = value;
 		}
-		}
-		cl(obj.innerProp)
-		cl(obj.innerProp = 'new value for obj')
+	}
+	obj.innerProp = 'new value for obj'
+	cl(obj.innerProp);
 }
-//objGetSet()
-function classGetSet(){
+objGetSet()
+function classGetSet() {
 	let privateValue = 'private'
-	class A{
-		get privateValue(){
-			cl('checking before get private');
+	class A {
+		get privateValue() {
+			cl(`some log/check/action before returning "${privateValue}"`);
 			return privateValue;
 		}
-		set privateValue(value){
-			cl('checking before set private ', value);
+		set privateValue(value) {
+			cl(`some log/check/action before setting "${privateValue}" to "${value}"`);
 			privateValue = value;
 		}
-		get someAsync(){
-		return	(async () => {
-				//res = await someAsAct()
-				return 'async value'
+		get someAsyncValue() {
+			return (async () => {
+				const asyncAction = new Promise((res) => setTimeout(res, 1000, 'value got async'))
+				return asyncAction
 			})()
 		}
 	}
 	const a = new A()
-	a.privateValue;
 	a.privateValue = 'new value to class'
-	a.someAsync.then(cl)
+	cl(a.privateValue);
+	a.someAsyncValue.then(cl)
 }
 classGetSet()
