19b2734892ab3442aa59d68bcff99ca49eedf36b
Oleh.Bondaruk <bondaruk9000@gmail.com>
2019-06-13 22:45:27 +0300

    added proxy

--- proxy/proxy.js
+++ proxy/proxy.js
@@ -1,9 +1,23 @@
 class ProxyClass {
 	constructor(){
 		this.someProperty = 'value';
-		
+	}
+	someMethod (arg) {
+		console.log(arg)
 	}
 }
 
-function makeProxy(target){}
+function makeProxy(target){
+	return new Proxy(target, {
+		set: (target, key, value) => {
+			if(key === 'someProperty'){
+				console.log('setting some prop value: ', value)
+				target[key] = value;
+			}
+		}
+	})
+}
 
+const proxy = makeProxy(new ProxyClass())
+proxy.someProperty = 'new value';
+console.log(proxy);
