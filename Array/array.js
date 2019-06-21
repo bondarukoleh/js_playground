10fd773451abd6e42d5321c548c0636c03e0a8bb
Oleh.Bondaruk <bondaruk9000@gmail.com>
2019-06-21 22:54:09 +0300

    make array from iterable

--- Array/array.js
+++ Array/array.js
@@ -1,3 +1,4 @@
+const cl = console.log;
 const sliceSplice = _ => {
   const a = ['a', 'b', 'c', 'd']
   console.log(a.slice(1, 3)); // [b, c]
@@ -5,3 +6,20 @@ const sliceSplice = _ => {
   console.log(a);
 }
 // sliceSplice()
+
+
+// make array from iterable object
+const o = {a: 1};
+const s = 'some string';
+
+function f(){	
+	const argsArraySlice = Array.prototype.slice.call(arguments);
+cl(Array.isArray(argsArraySlice));
+const arrFrom = Array.from(arguments);
+cl(Array.isArray(arrFrom));
+const arrDestr = [... arguments];
+cl(Array.isArray(arrDestr));
+}
+f(o, s)
+
+
