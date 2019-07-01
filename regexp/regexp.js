8e6c5433339c009f89210f5bbdddf17d6964baf5
Oleh.Bondaruk <bondaruk9000@gmail.com>
2019-07-02 01:05:28 +0300

    backreferences added

--- regexp/regexp.js
+++ regexp/regexp.js
@@ -6,5 +6,18 @@ const firstIndexOf3Digits = string.search(literalRegexp) // 5
 const replaced3Digits = string.replace(objectRegexp, '3DIGITS') // la1la3DIGITSlala3DIGITSd
 const found3Digits = string.match(objectRegexp) // [ '123', '654' ]
 const splitedBy3Digits = string.split(objectRegexp) // [ 'la1la', 'lala', 'd' ]
-const almostSameAsmatch = objectRegexp.exec(string) //[ '123', index: 5, input: 'la1la123lala654d', groups: undefined ]
+console.log(objectRegexp.exec(string)); //[ '123', index: 5, input: 'la1la123lala654d' ]
 const boolMatch = objectRegexp.test(string) //true
+
+const greedyRepetition = () => {
+  const str = 'Some sting. AAA repeats with AAA.'
+  greedyARegexp = /A+/;
+  nonGreedyARegexp = /A+?/;
+  greedyARegexpGlobal = /A+/g;
+  nonGreedyARegexpGlobal = /A+?/g;
+  console.log(str.match(greedyARegexp)); //[ 'AAA', index: 12, input: 'Some sting. AAA repeats with AAA.' ]
+  console.log(str.match(nonGreedyARegexp)); //[ 'A', index: 12, input: 'Some sting. AAA repeats with AAA.' ]
+  console.log(str.match(greedyARegexpGlobal)); //[ 'AAA', 'AAA' ]
+  console.log(str.match(nonGreedyARegexpGlobal)); //[ 'A', 'A', 'A', 'A', 'A', 'A' ]
+};
+greedyRepetition()
