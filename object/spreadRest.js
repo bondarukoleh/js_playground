288a6eccb886ef8aaffc217d1749b3b9985013b8
Oleh Bondaruk <bondaruk9000@gmail.com>
2020-02-25 22:02:31 +0100

    a few tricks with rest/spread

--- object/spreadRest.js
+++ object/spreadRest.js
@@ -27,3 +27,51 @@ const requiredParameterFunc = (needeParameter = throwError()) => console.log(nee
 console.log(Math.max(1, 2, ...[4, 5], 6));
 const map = new Map([[1, 'a'], [2, 'b']])
 console.log([...map]);
+
+function dynamicPropertyExtraction(){
+  /* Be aware! That it is not deletion, it is creation a new object, with removed getters and setters!!! */
+  const o = {user: 1, pass: 2};
+  const extractThe = (propertyToDelete) => ({[propertyToDelete]: _, ...rest}) => rest;
+
+  const deleteUserProperty = extractThe('user');
+  console.log(deleteUserProperty(o)) //{ pass: 2 }
+}
+// dynamicPropertyDeletion();
+
+function changePropertyOrder(){
+  const o = {second: 2, first: 1};
+  const reorder = (o) => ({first: undefined, second: undefined, ...o});
+
+  console.log(reorder(o)) //{ first: 1, second: 2 }
+}
+// changePropertyOrder();
+
+function setDefaults(){
+  const o = {second: 2, first: 1};
+  const o2 = {second: 2, first: 1, description: 'Set description.'};
+  const setDefaultDescription = ({description = 'Default description.', ...rest}) => ({...rest, description});
+
+  console.log(setDefaultDescription(o)); //{ second: 2, first: 1, description: 'Default description.' }
+  console.log(setDefaultDescription(o2)); //{ second: 2, first: 1, description: 'Set description.' }
+}
+// setDefaults();
+
+function renameProperties(){
+  const o = {USER_NAME: 2, pass: 1};
+  const renameNameProp = ({USER_NAME, ...rest}) => ({user: USER_NAME, ...rest});
+
+  console.log(renameNameProp(o)); //{ user: 2, pass: 1 }
+}
+// renameProperties();
+
+function addPropsWithCondition(){
+  const o = {user: 2, pass: 1};
+  const addPropsIfPassed = (o, props) => ({
+    ...o,
+    ...(props && {...props})
+  });
+
+  console.log(addPropsIfPassed(o, {desc: 'abc', additionProp: 'aaa'}));
+  // { user: 2, pass: 1, desc: 'abc', additionProp: 'aaa' }
+}
+// addPropsWithCondition();
