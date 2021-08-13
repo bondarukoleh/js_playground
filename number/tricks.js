296595a0a8a71fe8dcbb3bcef76a70997204fd08
Oleh Bondaruk <bondaruk9000@gmail.com>
2021-08-13 20:36:41 +0200

    added trick with numbers

--- number/tricks.js
+++ number/tricks.js
@@ -14,4 +14,23 @@ function checkFloatingPointProblem() {
   }
 }
 
-checkFloatingPointProblem();
+// checkFloatingPointProblem();
+
+function checkNumbers(){
+  // better check with static Number methods, they are more strict
+  console.log(isFinite(42)); // => true
+  console.log(isFinite('42')); // => true
+  console.log(Number.isFinite(42)); // => true // Returns true if n is not Infinity, -Infinity, or NaN
+  console.log(Number.isFinite('42')); // => false
+
+  console.log(isNaN(NaN));   // => true
+  console.log(isNaN('foo'));   // => true
+  console.log(Number.isNaN(NaN)); // => true
+  console.log(Number.isNaN('foo')); // => false
+
+  // but
+  const string = 'foo';
+  const nan = Number(string);
+  console.log(Number.isNaN(nan)); // => true
+}
+checkNumbers();
